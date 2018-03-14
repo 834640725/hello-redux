e8e2058b7bb4582bd464fdbec685952e60fa536f
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-03-14 22:25:28 +0800

    react redux

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers/counter';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
 // store.subscribe(() => console.log("State updated!", store.getState()));
 
-const render = () => {
-  ReactDOM.render(
-    <App
-      onIncrement={ () => store.dispatch({ type: "INCREMENT" }) }
-      onDecrement={ () => store.dispatch({ type: "DECREMENT" }) }
-      value={ store.getState() }
-    />, document.getElementById('root'));
-};
-
-render();
-
-store.subscribe(render);
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();

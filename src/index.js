256f83e6a810cd21f80cbf37d4dd069bdec77f52
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-03-18 15:52:47 +0800

    redux logger

--- src/index.js
+++ src/index.js
@@ -3,36 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 
-const logger = store => next => action => {
-  console.log('dispatching', action);
-  let result = next(action);
-  console.log('next state', store.getState());
-  return result;
-};
-
-const error = store => next => action => {
-  try {
-    next(action)
-  } catch(e) {
-    console.log('error ' + e);
-  }
-};
-
-// const logger = function(store) {
-//   return function(next) {
-//     return function(action) {
-//       console.log('dispatching', action);
-//       let result = next(action);
-//       console.log('next state', store.getState());
-//       return result;
-//     }
-//   }
-// }
-
-const store = createStore(rootReducer, {}, applyMiddleware(logger, error));
+const store = createStore(rootReducer, {}, applyMiddleware(logger));
 
 // store.subscribe(() => console.log("State updated!", store.getState()));
 
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,8 +1,7 @@
 const counter = (state = 1, action = {}) => {
   switch(action.type) {
     case 'INCREMENT':
-      throw new Error('error in INCREMENT')
-      // return state + 1;
+      return state + 1;
     case 'DECREMENT':
       return state - 1;
     default: return state;

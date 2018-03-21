b8acfd31d765df0e81dd6007098fd6971a66b3da
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-03-21 13:31:31 +0800

    config store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
+
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(logger, thunk, promise())));
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 // store.subscribe(() => console.log("State updated!", store.getState()));
 
